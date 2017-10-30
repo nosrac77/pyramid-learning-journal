20718a207a8967c3bb9e68b599a4404aa3ba66dc
nosrac77 <carson.newton@outlook.com>
2017-10-30 15:57:20 -0700

    added initial files

---
+++ learning_journal/views/default.py
@@ -0,0 +1,22 @@
+"""Module that contains callable server functions."""
+from pyramid.response import Response
+
+
+def list_view():
+    """Function that generates list of journal entries."""
+    return Response()
+
+
+def detail_view():
+    """Function that generates single journal entry."""
+    return Response()
+
+
+def create_view():
+    """Function that generates new view."""
+    return Response()
+
+
+def update_view():
+    """Function that updates existing view."""
+    return Response()
