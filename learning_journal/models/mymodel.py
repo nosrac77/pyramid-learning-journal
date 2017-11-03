bb0ad4d0d6387144a4d65881c8f14dfe2cc62841
nosrac77 <carson.newton@outlook.com>
2017-11-03 10:17:14 -0700

    modified sqlalchemy url

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -1,18 +1,26 @@
 from sqlalchemy import (
     Column,
-    Index,
+    Unicode,
     Integer,
     Text,
+    DateTime
 )
 
 from .meta import Base
+from datetime import datetime
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class Journal(Base):
+    __tablename__ = 'journals'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    title = Column(Unicode)
+    creation_date = Column(DateTime)
+    body = Column(Text)
+
+    def __init__(self, *args, **kwargs):
+        """Modify instance of Journal class."""
+        super(Journal, self).__init__(*args, **kwargs):
+            self.creation_date = datetime.now()
 
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+# Index('my_index', MyModel.name, unique=True, mysql_length=255)
